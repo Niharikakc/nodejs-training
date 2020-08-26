2c8a8a880a1a544fd34f2a87f5f512330fa7c128
Niharikakc <niharikakc47@gmail.com>
2020-08-26 15:09:44 +0530

    assignment1

--- assignment01/1.js
+++ assignment01/1.js
@@ -1,9 +1,9 @@
-function isPrime(n) {
-    for (var i = 2; i < n; i++) { 
-      if(n % i === 0) return false; 
+function isPrimeNumber(num) {
+    for(var i = 2; i < num; i++){
+      if(num % i === 0) return false;
     }
-    return n > 1; 
+    return num > 1;
   }
   
-  console.log(isPrime(1));  
+  console.log(isPrimeNumber(4));  
 
